b45c7b0e4048e2499b95b980d0fbfdc50a5dfd11
sahalfreelance <110083545+sahalfreelance@users.noreply.github.com>
2024-11-11 05:00:20 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt install python3
+sudo apt install python3-pip
+pip3 install requests
+pip3 install json
+pip3 install loguru
+pip3 install asyncio
+pip3 install fake_useragent
+clear
+python3 teneo.py
